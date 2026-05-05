5e8a602bff78d26e8f3a622f24e5a36e0d995332
islembriki <islembriki684@gmail.com>
2026-05-06 00:55:09 +0100

    testing initial2

--- src/main/java/com/tp3/portailetudiants/controller/TestController.java
+++ src/main/java/com/tp3/portailetudiants/controller/TestController.java
@@ -7,23 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 
-    // Root path - displays welcome.html (Thymeleaf)
-    @GetMapping("/")
+    // On teste la racine du site
+    @GetMapping("/hello")
     public String index(Model model) {
         model.addAttribute("message", "BRAVO ! Spring 6 fonctionne !");
-        return "welcome"; // Returns WEB-INF/templates/welcome.html
-    }
-
-    // Mapping for test-xml.jsp
-    @GetMapping("/test-xml")
-    public String testXml() {
-        return "test-xml"; // Returns WEB-INF/jsp/test-xml.jsp
-    }
-
-    // Mapping for welcome.jsp
-    @GetMapping("/welcome-jsp")
-    public String welcomeJsp(Model model) {
-        model.addAttribute("message", "Welcome to JSP Page!");
-        return "welcome"; // Returns WEB-INF/jsp/welcome.jsp
+        return "welcome"; // Va chercher WEB-INF/jsp/welcome.jsp
     }
 }
